6e2eeccebb5e02d55e280592c4ce5e12bb042165
zuchzub <kumsalmuzik07@gmail.com>
2025-10-23 19:25:44 +0300

    Update calls.go

--- pkg/vc/ubot/calls.go
+++ pkg/vc/ubot/calls.go
@@ -1,7 +1,7 @@
 package ubot
 
 import (
-	"https://github.com/iamnolimit/tggomusicbot/pkg/vc/ntgcalls"
+	"https://github.com/zuchzub/Go/pkg/vc/ntgcalls"
 
 	tg "github.com/amarnathcjd/gogram/telegram"
 )
